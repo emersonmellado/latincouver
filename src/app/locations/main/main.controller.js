f6a3c108194f64c224c100f3832285ea78ec4eeb
Emerson Mellado <emersonmellado@gmail.com>
2016-05-29 15:50:33 -0300

    Adjustments

--- src/app/locations/main/main.controller.js
+++ src/app/locations/main/main.controller.js
@@ -1,7 +1,22 @@
 class MainController {
-    constructor($state,$scope, $ionicSideMenuDelegate) {
-        this.$state = $state;  
-        this.$ionicSideMenuDelegate = $ionicSideMenuDelegate;    
+    constructor($state, $scope, $ionicSideMenuDelegate) {
+        this.$state = $state;
+        this.$ionicSideMenuDelegate = $ionicSideMenuDelegate;
+        this.sideMenu = [
+            { title: 'Main Stage', sref: 'mainStage' },
+            { title: 'Plazas', sref: 'plazas' },
+            { title: 'Vendors', sref: 'vendors' },
+            { title: 'Map', sref: 'map' },
+            { title: 'FAQ', sref: 'faq' },
+            { title: 'Contest', sref: 'contest' },
+            { title: 'Donate', sref: 'donate' },
+            { title: 'Media (#shinetogether)', sref: 'media' },
+            { title: 'Sponsors and Partners', sref: 'sponsors' },
+            { title: 'Latincouver Main Menu', sref: 'main' }
+        ];
+
+        this.name = "latincouver"; 
+        this.obj = "app"; 
     }
     toIntro() {
         window.localStorage.didTutorial = "false";
@@ -10,7 +25,7 @@ class MainController {
     }
     toggleLeft() {
         this.$ionicSideMenuDelegate.toggleLeft();
-    }  
+    }
 }
-MainController.$inject = ['$state','$scope','$ionicSideMenuDelegate'];
+MainController.$inject = ['$state', '$scope', '$ionicSideMenuDelegate'];
 export default MainController;
